a5346ea0697696e37a679f7c825abb7a5e3a8ba3
ch04937 <97hernandez.c@gmail.com>
2023-06-03 21:04:23 -0500

    no logs

--- src/math/index.ts
+++ src/math/index.ts
@@ -3,8 +3,5 @@ import { add } from "./add";
 import { subtract } from "./subtract";
 import { multiply } from "./multiply";
 
-console.log("mutiply", multiply(2, 5));
-console.log("add", add(2, 5));
-console.log("add", subtract(2, 5));
 // exports
 export { multiply, add, subtract };
