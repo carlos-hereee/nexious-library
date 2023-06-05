420c4ca1312d6d64d7501096e775a65f51543999
ch04937 <97hernandez.c@gmail.com>
2023-06-05 16:01:18 -0500

    to do fix import error

--- example/main.js
+++ example/main.js
@@ -1,1 +1,14 @@
-const nexious = require("nexious-library/");
+import {
+  Button,
+  add,
+  Capitalize,
+  Heading,
+  Hyperlink,
+  Icon,
+  Navlink,
+  Title,
+  multiply,
+  subtract,
+} from "nexious-library";
+
+console.log("add(3, 6) => 9", add(3, 6));
