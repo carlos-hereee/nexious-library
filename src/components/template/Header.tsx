590ccb7f5e9b060ff4d35a91c39abe9a93a9bd3a
ch04937 <97hernandez.c@gmail.com>
2023-06-23 21:28:22 -0500

    add event listener for end of animation

--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { BurgerButton, Logo } from "@nxs-molecules";
 import { MenuItemProp, Navbar } from "@nxs-organism";
@@ -6,6 +6,7 @@ import { MenuItemProp, Navbar } from "@nxs-organism";
 export type HeaderProps = {
   data: { url: string; alt?: string; title: string };
   menu: MenuItemProp[];
+  title: string;
 };
 /**
  * Component - Header
@@ -20,7 +21,7 @@ export type HeaderProps = {
  * @param menu.isToggle boolean;
  * @returns Header component
  */
-const Header: React.FC<HeaderProps> = ({ menu, data }) => {
+const Header: React.FC<HeaderProps> = ({ menu, data, title }) => {
   const [isActive, setActive] = useState(false);
   const [isClose, setClose] = useState(false);
   const [items, setItems] = useState(menu || []);
@@ -33,10 +34,15 @@ const Header: React.FC<HeaderProps> = ({ menu, data }) => {
     setItems(toggle);
   };
 
+  useEffect(() => {
+    const initClose = () => setClose(true);
+    document.addEventListener("animationend", initClose, true);
+    return () => document.removeEventListener("animationend", initClose, true);
+  }, []);
   return (
     <header>
       <Link to="/" className="navlink">
-        <Logo data={data} title={data.title} />
+        <Logo data={data} title={title} />
       </Link>
       <nav className="primary-navigation">
         <Navbar
