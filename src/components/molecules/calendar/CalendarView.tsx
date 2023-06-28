37933358a4f4f53a4c532b2e08315c0ed5092cd0
ch04937 <97hernandez.c@gmail.com>
2023-06-28 13:12:53 -0500

    add calendar view days

--- src/components/molecules/calendar/CalendarView.tsx
+++ src/components/molecules/calendar/CalendarView.tsx
@@ -1,4 +1,23 @@
+import { week, monthDays } from "@nxs-helpers/data";
+
 const CalendarView = () => {
-  return <div>CalendarView</div>;
+  return (
+    <div className="calendar-view">
+      <div className="calendar-week flex-g">
+        {week.map((w) => (
+          <span key={w} className="day">
+            {w}
+          </span>
+        ))}
+      </div>
+      <div className="flex-w">
+        {monthDays.map((md) => (
+          <button key={md} className="btn btn-calendar-tile">
+            {md}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default CalendarView;
--- src/helpers/data.tsx
+++ src/helpers/data.tsx
@@ -12,7 +12,7 @@ export const months = [
   "November",
   "December",
 ];
-export const days = [
+export const week = [
   "monday",
   "tuesday",
   "wednesday",
@@ -21,3 +21,8 @@ export const days = [
   "saturday",
   "sunday",
 ];
+export const monthDays = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+];
+export const weekDays = [1, 2, 3, 4, 5, 6, 7];
