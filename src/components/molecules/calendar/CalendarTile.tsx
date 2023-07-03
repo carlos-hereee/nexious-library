228cf2064854ce09acc2b7bd8fe98f9bd9c3876f
ch04937 <97hernandez.c@gmail.com>
2023-07-03 16:06:38 -0500

    create calendar tile

---
+++ src/components/molecules/calendar/CalendarTile.tsx
@@ -0,0 +1,19 @@
+import { Button, TileContent } from "@nxs-atoms";
+
+export type CalendarTileProps = {
+  data: { tile: number; muted: boolean };
+  events?: any[];
+  click: () => void;
+};
+const CalendarTile: React.FC<CalendarTileProps> = (props) => {
+  const { events, click, data } = props;
+  return (
+    <Button name="calendar-tile" click={click}>
+      <span className={data.muted ? "text-mute" : ""}>{data.tile}</span>
+      {events && events?.includes(data.tile) && (
+        <TileContent tile={events.filter((e) => e === data.tile).length} />
+      )}
+    </Button>
+  );
+};
+export default CalendarTile;
