4afbf886bf2c938e4d5a87ac2d09fa7204d37741
ch04937 <97hernandez.c@gmail.com>
2023-07-15 17:14:50 -0500

    update optional props

--- src/components/organism/card/HeroCard.tsx
+++ src/components/organism/card/HeroCard.tsx
@@ -2,13 +2,13 @@ import { Hero, Btn } from "@nxs-atoms/index";
 
 type HeroCardProps = {
   heading: string;
-  tagline: string;
+  tagline?: string;
   hero: {
     url: string;
     alt: string;
     name: string;
   };
-  cta: string;
+  cta?: string;
 };
 const HeroCard: React.FC<HeroCardProps> = (props) => {
   const { heading, tagline, hero, cta } = props;
@@ -16,10 +16,12 @@ const HeroCard: React.FC<HeroCardProps> = (props) => {
     <div className="hero-card">
       <div className="hero-card-header">
         <h1 className="hero-card-heading">{heading}</h1>
-        <h2 className="tagline">{tagline}</h2>
-        <Btn name="cta" title={cta}>
-          {cta}
-        </Btn>
+        {tagline && <h2 className="tagline">{tagline}</h2>}
+        {cta && (
+          <Btn name="cta" title={cta}>
+            {cta}
+          </Btn>
+        )}
       </div>
       <div className="hero-container">
         <Hero hero={hero} name={hero.name} />
