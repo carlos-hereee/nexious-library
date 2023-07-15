e02dec55268b9f9077e3a820e72d7da007fabce0
ch04937 <97hernandez.c@gmail.com>
2023-07-15 16:38:33 -0500

    toggle rating + css

--- src/components/molecules/assets/Rating.tsx
+++ src/components/molecules/assets/Rating.tsx
@@ -2,15 +2,23 @@ import { Icon } from "@nxs-atoms/index";
 
 type RatingProps = {
   star: number;
+  click?: (e: any) => void;
 };
 const Rating: React.FC<RatingProps> = (props) => {
-  const { star } = props;
+  const { star, click } = props;
   const ratings = [1, 2, 3, 4, 5];
   return (
     <div className="rating">
       <strong>{star} / 5 </strong>{" "}
       {ratings.map((r) => (
-        <Icon icon="star" name={star < r ? "star" : "star-filled"} key={r} />
+        <button
+          key={r}
+          className="btn btn-rating"
+          type="button"
+          onClick={() => click && click(r)}
+        >
+          <Icon icon="star" name={star < r ? "star" : "star-filled"} />
+        </button>
       ))}
     </div>
   );
