7f7d35ac12ac3c2cba226adfd92e29a4da30f8b7
ch04937 <97hernandez.c@gmail.com>
2023-07-15 20:27:00 -0500

    update style move showmore button to the right

--- src/components/atoms/texts/ReadMore.tsx
+++ src/components/atoms/texts/ReadMore.tsx
@@ -5,9 +5,9 @@ const ReadMore: React.FC<ReadMoreProps> = ({ data }) => {
   const show = data.split(".");
   const target = show.map((s, idx) => (idx >= 1 ? s : "")).join(".");
   return (
-    <div>
+    <div className="read-more-wrapper">
       <input type="checkbox" className="read-more-state" id="post" />
-      <p className="read-more-wrap">
+      <p className="read-more-wrap p-stretch">
         {show[0]}
         <span className="read-more-target">{target}</span>
       </p>
