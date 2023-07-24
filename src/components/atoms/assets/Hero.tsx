a94bfb7d3ec3e28512174b97147211504d7dd739
ch04937 <97hernandez.c@gmail.com>
2023-07-24 13:39:59 -0500

    add pulse keyframe

--- src/components/atoms/assets/Hero.tsx
+++ src/components/atoms/assets/Hero.tsx
@@ -1,5 +1,6 @@
 import { HeroProp } from "@nxs-helpers/types";
 import { capFirstChar } from "@nxs-utils/text";
+import { useState } from "react";
 
 type Props = { hero: HeroProp; name?: string };
 
@@ -11,6 +12,7 @@ type Props = { hero: HeroProp; name?: string };
  * @returns image component
  */
 const Hero: React.FC<Props> = ({ hero, name }) => {
+  const [load, setLoad] = useState<boolean>();
   const isCreditNeeded = hero.url.includes("unsplash");
   const artistName = hero.credit?.artistName
     .split("-")
@@ -18,11 +20,19 @@ const Hero: React.FC<Props> = ({ hero, name }) => {
     .join(" ");
 
   return isCreditNeeded ? (
-    <div className="credit-unsplash">
+    <div
+      className={
+        load
+          ? "credit-unsplash blur-load blur-load--loaded"
+          : "credit-unsplash blur-load"
+      }
+    >
       <img
+        loading="lazy"
         className={name ? `hero hero-${name}` : "hero"}
         src={hero.url}
         alt={hero.alt}
+        onLoad={() => setLoad(true)}
       />
       <p className="credit-to">
         Photo by <a href={hero.credit?.artistUrl}>{artistName}</a> on{" "}
@@ -30,11 +40,15 @@ const Hero: React.FC<Props> = ({ hero, name }) => {
       </p>
     </div>
   ) : (
-    <img
-      className={name ? `hero hero-${name}` : "hero"}
-      src={hero.url}
-      alt={hero.alt}
-    />
+    <div className={load ? "blur-load blur-load--loaded" : "blur-load"}>
+      <img
+        loading="lazy"
+        className={name ? `hero hero-${name}` : "hero"}
+        src={hero.url}
+        alt={hero.alt}
+        onLoad={() => setLoad(true)}
+      />
+    </div>
   );
 };
 
