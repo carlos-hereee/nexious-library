2d9aa5bb72e402cdc8252afc8ac627a3249e58a7
ch04937 <97hernandez.c@gmail.com>
2023-07-28 20:21:34 -0500

    toggle visible password

--- src/components/organism/forms/Form.tsx
+++ src/components/organism/forms/Form.tsx
@@ -11,15 +11,19 @@ type FormProps = {
   stretchInput?: boolean;
   name?: string;
   type?: string;
-  isAuth?: boolean;
 };
 const Form: React.FC<FormProps> = (props) => {
-  const { submit, type, values, hideLabels, name, stretchInput, isAuth } =
-    props;
+  const { submit, type, values, hideLabels, name, stretchInput } = props;
   const [value, setValue] = useState<{ [key: string]: string | number }>(
     values
   );
   const [errors, setErros] = useState<{ [key: string]: string | number }>();
+  const [seePassword, setSeePassword] = useState<{ [key: string]: boolean }>({
+    password: false,
+    confirmPassword: false,
+  });
+  const auth = ["password", "confirmPassword"];
+
   const handleChange = (e: any) => {
     const key = e.target.name;
     const val = e.currentTarget.value;
@@ -45,6 +49,7 @@ const Form: React.FC<FormProps> = (props) => {
     const { isValidated, errors } = validateForm(e.target);
     isValidated ? submit(value) : setErros(errors);
   };
+
   return (
     <form
       className={`form${name ? ` ${name}` : ""} ${
@@ -56,23 +61,47 @@ const Form: React.FC<FormProps> = (props) => {
         <div key={v} className="form-field">
           {!hideLabels && (
             <label htmlFor={v} className="label">
-              {" "}
               {labels[v]}: <br />
               {errors && errors[v] && (
                 <span className="required">{errors[v]}</span>
               )}
             </label>
           )}
-          <input
-            type={types[typeof labels[v]]}
-            autoComplete="on"
-            name={v}
-            value={value[v] || ""}
-            placeholder={placeholders[v]}
-            onChange={handleChange}
-            // onBlur={handleBlur}
-            className="input"
-          />
+          {auth.includes(v) ? (
+            <div className="flex">
+              <input
+                type={seePassword[v] ? "password" : "text"}
+                autoComplete="on"
+                name={v}
+                placeholder={placeholders[v]}
+                onChange={handleChange}
+                className="password"
+              />
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={() =>
+                  setSeePassword({ ...seePassword, [v]: !seePassword[v] })
+                }
+              >
+                {seePassword[v] ? (
+                  <Icon icon="eye" />
+                ) : (
+                  <Icon icon="eyeSlash" />
+                )}
+              </button>
+            </div>
+          ) : (
+            <input
+              type={types[typeof labels[v]]}
+              autoComplete="on"
+              name={v}
+              value={value[v] || ""}
+              placeholder={placeholders[v]}
+              onChange={handleChange}
+              // onBlur={handleBlur}
+            />
+          )}
         </div>
       ))}
       <button type="submit" className="btn btn-main">
