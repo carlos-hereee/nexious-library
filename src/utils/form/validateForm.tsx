9f6823148f837f5ea6895405fa9323a72a18d4bc
ch04937 <97hernandez.c@gmail.com>
2023-07-28 23:58:08 -0500

    add is password strong

--- src/utils/form/validateForm.tsx
+++ src/utils/form/validateForm.tsx
@@ -12,6 +12,34 @@ const validateEmail = (mail: string) => {
   }
   return { isMailValidated };
 };
+const matchingPassword = (password: string, confirmPassword: string) => {
+  return password === confirmPassword;
+};
+const isPasswordStrong = (password: string) => {
+  let strength = 0;
+  let tips = [""];
+  // check length
+  if (password.length < 8) {
+    const len = 8 - password.length;
+    tips.push(
+      `The length of strong passwords start at 8 characters long! ${len} character(s) left`
+    );
+  } else strength += 1;
+  // check for caps case
+  if (password.match(/[a-z]/) && password.match(/[A-Z]/)) {
+    tips.push("Use lowercase and uppercase letters.");
+  } else strength += 1;
+  // check numbers
+  if (password.match(/\d/)) {
+    strength += 1;
+  } else tips.push("Include at least one number.");
+  // check for special characters
+  if (password.match(/[^a-zA-Z\d]/)) {
+    strength += 1;
+  } else tips.push("Include at least one special charater.");
+
+  return { strength, tips };
+};
 export const validateForm = (values: ValidateFormProps) => {
   let isValidated: boolean = true;
   let errors: { [key: string]: string } = {};
@@ -27,6 +55,19 @@ export const validateForm = (values: ValidateFormProps) => {
       if (!isMailValidated) {
         isValidated = false;
         errors[key] = labels[key] + " please provide a valid email address";
+      }
+    }
+    // check password strength
+    if (key === "password") {
+      if (isPasswordStrong(values[key])) {
+        console.log("name");
+      }
+    }
+    // confirm matching passwords
+    if (key === "confirmPassword") {
+      if (!matchingPassword(values["password"], values[key])) {
+        isValidated = false;
+        errors[key] = labels[key] + " must match password";
       }
     }
   });
