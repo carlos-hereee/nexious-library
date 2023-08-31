487cb9a8f2f5b0fc0c4b5ea045a4066d2f939b4e
ch04937 <97hernandez.c@gmail.com>
2023-08-30 19:06:18 -0500

    add cart row

--- src/components/molecules/card/CartRow.tsx
+++ src/components/molecules/card/CartRow.tsx
@@ -1,40 +1,19 @@
-import { ButtonCancel, ReadMore, Cost, MeetingDetails } from "@nxs-atoms";
+import { ReadMore } from "@nxs-atoms";
 import CardHeader from "./CardHeader";
+import { CardProp } from "@nxs-utils/helpers/types";
+import { CardFooter, IconButton } from "..";
 
 type CartRowProps = {
-  data: any;
-  active?: string;
-  service?: any;
-  meeting?: any;
-  setCancel: () => void;
-  setActive: (value: any) => void;
-  editDetails: () => void;
+  data: CardProp;
+  click: (value: any) => void;
 };
 const CartRow: React.FC<CartRowProps> = (props) => {
-  const { data, setCancel, setActive, active, service, editDetails, meeting } =
-    props;
+  const { data, click } = props;
   return (
-    <div
-      className={
-        active === service.uid
-          ? "flex-d-column card-row--active"
-          : "flex-d-column card-row-container"
-      }
-    >
-      <button className="card-row" type="button" onClick={setActive}>
-        <CardHeader data={service} isRow />
-        <div className="card-row-body">
-          {service.body && <ReadMore data={service.body} uid={service.uid} />}
-          {meeting.uid && <MeetingDetails meeting={meeting} />}
-          {service.cost && <Cost data={service.cost} />}
-        </div>
-      </button>
-      <div className="card-row-footer">
-        <ButtonCancel click={setCancel} data="Cancel" />
-        <button className="btn-main" type="button" onClick={editDetails}>
-          Edit Details
-        </button>
-      </div>
+    <div className="cart-row">
+      <CardHeader data={data} isRow />
+      {data.body && <ReadMore data={data.body} uid={data.uid} />}
+      {data.cta && <CardFooter data={data.cta} click={click} />}
     </div>
   );
 };
