4c2d233b635114ebe99bb358886a71b5c34a8c00
ch04937 <97hernandez.c@gmail.com>
2023-09-21 22:40:23 -0500

    display and toggle prev and next buttons

--- src/components/template/PaginateForm.tsx
+++ src/components/template/PaginateForm.tsx
@@ -3,11 +3,12 @@ import { FormInitValues, KeyStringProp } from "@nxs-utils/helpers/types";
 import Form from "../organism/forms/Form";
 import { usePropErrorHandling } from "@nxs-utils/hooks/usePropErrorHandling";
 import { ErrorMessages } from "@nxs-molecules/index";
+import { Button } from "@nxs-atoms/index";
 
 type PaginateFormProps = {
   // required props
   page: number;
-  setNewPage: (e: any) => void;
+  setNewPage: (e: number) => void;
   onFormSubmit: (e: any) => void;
   paginate: [
     {
@@ -39,26 +40,27 @@ const PaginateForm: React.FC<PaginateFormProps> = (props) => {
   const formOrder = order
     ? order
     : paginate.map((form) => (form.formName ? form.formName : ""));
+  const total = formOrder.length;
   const formName = paginate[page].formName;
-  const theme = paginate[page].theme;
-  const labels = paginate[page].labels;
-  const submitLabel = paginate[page].submitLabel;
-  const placeholders = paginate[page].placeholders;
-  const types = paginate[page].types;
   const heading = paginate[page].heading;
-  const schema = paginate[page].schema;
-  // init form prop values
-  const [initialValues, setInitialValues] = useState<FormInitValues>();
-  // store form values
   const [values, setValues] = useState<FormInitValues>({});
+  // const theme = paginate[page].theme;
+  // const labels = paginate[page].labels;
+  // const submitLabel = paginate[page].submitLabel;
+  // const placeholders = paginate[page].placeholders;
+  // const types = paginate[page].types;
+  // const schema = paginate[page].schema;
+  // init form prop values
+  // const [initialValues, setInitialValues] = useState<FormInitValues>();
+  // store form values
 
-  useEffect(() => {
-    if (page >= 0) {
-      // reset initial values to redender form component
-      setInitialValues(undefined);
-      setInitialValues(paginate[page].initialValues);
-    }
-  }, [page]);
+  // useEffect(() => {
+  //   if (page >= 0) {
+  //     // reset initial values to redender form component
+  //     setInitialValues(undefined);
+  //     setInitialValues(paginate[page].initialValues);
+  //   }
+  // }, [page]);
 
   const handlePaginateSubmit = (formValues: FormInitValues) => {
     // search next in the order
@@ -66,37 +68,51 @@ const PaginateForm: React.FC<PaginateFormProps> = (props) => {
     // if next is undefined its the last form
     if (!next) {
       onFormSubmit({ ...values, [formName]: { ...formValues } });
-      setInitialValues(undefined);
+      // setInitialValues(undefined);
     } else {
       const nextPage = paginate.findIndex(
         (form) => form.formName === formOrder[page + 1]
       );
-      // reset initial values to redender form component
-      setInitialValues(undefined);
+      // // reset initial values to redender form component
+      // setInitialValues(undefined);
       setNewPage(nextPage);
       // save form values
       setValues({ ...values, [formName]: { ...formValues } });
     }
   };
+  const prevPage = () => setNewPage(page === 0 ? page : page - 1);
+  const nextPage = () => setNewPage(page + 1 === total ? page : page + 1);
 
   if (lightColor === "red") {
     return <ErrorMessages errors={errors} component="PaginateForm" />;
   }
+  console.log("initialValues", paginate[page].initialValues);
   return (
-    initialValues && (
-      <Form
-        initialValues={initialValues}
-        onSubmit={(event) => handlePaginateSubmit(event)}
-        labels={labels}
-        placeholders={placeholders}
-        submitLabel={submitLabel}
-        types={types}
-        theme={theme}
-        schema={schema}
-        formName={formName}
-        heading={heading}
-      />
-    )
+    <div className="container">
+      <div>
+        <p>
+          Form {page + 1} out of {total}
+        </p>
+        <div className="flex-row">
+          <Button name="previous" label="previous" handleClick={prevPage} />
+          <Button name="next" label="next" handleClick={nextPage} />
+        </div>
+        {heading && <h2 className="heading">{heading}</h2>}
+      </div>
+      {paginate[page].initialValues && (
+        <Form
+          initialValues={paginate[page].initialValues}
+          onSubmit={(event) => handlePaginateSubmit(event)}
+          labels={paginate[page].labels}
+          placeholders={paginate[page].placeholders}
+          submitLabel={paginate[page].submitLabel}
+          types={paginate[page].types}
+          theme={paginate[page].theme}
+          schema={paginate[page].schema}
+          formName={paginate[page].formName}
+        />
+      )}
+    </div>
   );
 };
 export default PaginateForm;
--- src/utils/form/labels.tsx
+++ src/utils/form/labels.tsx
@@ -31,6 +31,7 @@ export const initLabels: { [key: string]: string } = {
   label: "Display name:",
   body: "Data content:",
   title: "Title:",
+  link: "Enter page name this link points to",
 };
 export const getLabel = (key: string, labels?: KeyStringProp) => {
   return labels && labels[key] ? labels[key] : initLabels[key];
