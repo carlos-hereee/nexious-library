67a47612b2a97c0093cacae8c0fb481332909f6b
ch04937 <97hernandez.c@gmail.com>
2023-09-24 15:25:27 -0500

    create function to search files in path

--- scripts/renameFile.sh
+++ scripts/renameFile.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+# # Total number of arguments
+# num_args="$#"
+# # All arguments as a single string
+# all_args="$*"
+# # All arguments as an array
+# args_array=("$@")
+# # # Accessing the filename
+# filename="$0"
 
 # KEY VARIABLES
-# Total number of arguments
-num_args="$#"
-# All arguments as a single string
-all_args="$*"
-# All arguments as an array
-args_array=("$@")
-# # Accessing the filename
-filename="$0"
 # Get the current directory path
 current_dir=$(pwd)
+node_module="node_modules/nexious-library/"
 # # Accessing the first argument PATH
-path="$1"
+input_path=""$current_dir"/""$1"
+# # remove node_modules from string if exists
+path="${input_path//"$node_module"/}"
 # # Accessing the second argument
 pattern="$2"
 # # Accessing the third argument
@@ -24,7 +27,7 @@ require_path() {
   # # -z is the test operator to check if path is empty
   if [ -z "$path" ]; then
     echo "Error Occured: first argument is not provided"
-    echo "Posible Fix: update first arg. value to desired schema ./path/to/directory"
+    echo "Posible Fix: update first arg. value to desired schema path or path/to/directory"
     # Exit Code 1: This is often used to indicate a general error.
     # If a program or script encounters an unspecified or unexpected issue,
     # it may return an exit code of 1 to signal a problem without providing specific details.
@@ -36,11 +39,39 @@ require_path() {
       echo "No directory found on path "$path""
       return 1
     else
-      echo "Directory found on path "$path""
+      # passed all errors start next process
+      echo "Found directory on path "$path""
     fi
-
   fi
+}
+# Define function for searching files in a directory
+require_pattern() {
+  # Check if second argument is provided
+  if [ -z "$pattern" ]; then
+    echo "Error Occured:  second argument is not provided"
+    echo "Old file extension name is $pattern"
+    echo "Posible Fix: change value to desired schema 'jsx' or similar"
+    # Exit Code 1: This is often used to indicate a general error.
+    return 1
+  fi
+}
+# Search for files that match search critiria and count them
+search_files() {
+  local count
+  count=$(find "$path" -type f -name "*.{$pattern}" | wc -l)
+  # if any matching files were found
+  if [ "$count" -gt 0 ]; then
+    # do things
+    echo "Files found "$count" matching the search criteria "$pattern""
+  else
+    # No files found
+    echo ""$count" files found matching search criteria"
+    # echo "You are in the directory: "$path""
+    return 1
+  fi
+}
 
+require_new_name() {
   # Check if third argument is provided
   if [ -z "$newName" ]; then
     echo "Error Occured:  thrid argument is not provided"
@@ -51,35 +82,10 @@ require_path() {
   else
     echo "Found arg. new file extensions is $newName"
   fi
-  # passed all errors start next process
-  echo "Searching directory $path"
 }
-# Define function for searching files in a directory
-search_directory() {
-  # Check if second argument is provided
-  if [ -z "$pattern" ]; then
-    echo "Error Occured:  second argument is not provided"
-    echo "Old file extension name is $pattern"
-    echo "Posible Fix: change value to desired schema 'jsx' or similar"
-    # Exit Code 1: This is often used to indicate a general error.
-    return 1
-  else
-    # Search for files that match search critiria and count them
-    local count
-    count=$(find "$path" -type f -name "*.{$pattern}" | wc -l)
-    # if any files were found
-    if [ "$count" -gt 0 ]; then
-      # do things
-      echo "Files found "$count" matching the search criteria"
-    else
-      # No files found
-      echo ""$count" files found matching search criteria"
-      echo "You are in the directory: "$current_dir""
-      return 1
-    fi
-  fi
-}
+
 # error handling first
 require_path ""
-# search directory
-# search_directory ""
+require_pattern ""
+search_files ""
+# require_new_name ""
