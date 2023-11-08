569facc713454ef3091aa84fdad39a43aef4b791
carlos-hereee <97hernandez.c@gmail.com>
2023-11-07 23:31:24 -0600

    add section styling

--- src/utils/data/urlFile.tsx
+++ src/utils/data/urlFile.tsx
@@ -1,4 +1,5 @@
-export const urlFile = (file?: File) => {
+export const urlFile = (file?: File | string) => {
   if (!file) return "";
+  if (typeof file === "string") return file;
   return URL.createObjectURL(file);
 };
