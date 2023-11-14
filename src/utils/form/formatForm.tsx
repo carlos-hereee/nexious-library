75fb1fd8f6fa508db3e417257202fe4cac7a2344
carlos-hereee <97hernandez.c@gmail.com>
2023-11-14 03:52:38 -0600

    format form data

--- src/utils/form/formatForm.tsx
+++ src/utils/form/formatForm.tsx
@@ -1,25 +1,16 @@
 import { FieldValueProps } from "nxs-form";
 
 export const formatFormData = (values: FieldValueProps[]) => {
-  let payload: { [key: string]: any } = {};
-  const uniqueGroups: { [key: string]: string[] } = {};
-  values.forEach((val) => {
-    // check if value is part of a group
-    if (val.group) {
-      const { group, sharedKey, name, value, groupName } = val;
-      if (sharedKey && !uniqueGroups[group]?.includes(sharedKey)) {
+  return Object.assign(
+    {},
+    ...values.map((val) => {
+      if (val.group) {
+        const { group, sharedKey, name, value, groupName } = val;
         const groupPayload = { value, name, sharedKey, group, groupName };
-        // check if the group has not been checked
-        if (uniqueGroups[group] && !uniqueGroups[group].includes(sharedKey)) {
-          // if not checked add to uniqueGroups; create new instance
-          uniqueGroups[group] = [...uniqueGroups[group], sharedKey];
-        }
-        if (payload[group]) payload[group] = [...payload[group], groupPayload];
-        else payload[group] = [groupPayload];
-      }
-    } else payload[val.name] = val.value;
-  });
-  return payload;
+        return { [val.name]: groupPayload };
+      } else return { [val.name]: val.value };
+    })
+  );
 };
 export const formatFilesData = (values: FieldValueProps[]) => {
   // form data is tricky it wont show values in console, send to db and check there
