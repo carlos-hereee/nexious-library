39159557af8dcbd801f82019736b2c77eb53de11
carlos-hereee <97hernandez.c@gmail.com>
2023-11-21 00:51:58 -0600

    add type checking to utils

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-export { default as Header } from "@nxs-template/Header";
 export { default as Footer } from "@nxs-template/Footer";
+export { default as Header } from "@nxs-template/Header";
 export { default as Calendar } from "@nxs-template/Calendar";
 export { default as PaginateForm } from "@nxs-template/PaginateForm";
 export { default as CalendarEvents } from "@nxs-template/CalendarEvents";
