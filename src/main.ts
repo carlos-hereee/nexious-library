264f67a209538b9bf91acb4c61faa3ae56d82428
carlos-hereee <97hernandez.c@gmail.com>
2024-05-04 20:14:24 -0500

    add popular components to main entry

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,20 @@ export { default as Form } from "@nxs-template/Form";
 export { default as CalendarEvents } from "@nxs-template/CalendarEvents";
 export { default as Dialog } from "@nxs-template/Dialog";
 export { default as ItemDetail } from "@nxs-template/ItemDetail";
+
+// uniqueId
+export { uniqueId } from "@nxs-utils/data/uniqueId";
+export { combineArraysWithOutDups } from "@nxs-utils/app/combineArraysWithOutDups";
+
+// popular components
+export { default as Button } from "@nxs-atoms/buttons/Button";
+export { default as Hero } from "@nxs-molecules/assets/Hero";
+export { default as Loading } from "@nxs-molecules/errors/Loading";
+export { default as Icon } from "@nxs-atoms/assets/Icon";
+export { default as IconButton } from "@nxs-molecules/buttons/IconButton";
+export { default as HeroCard } from "@nxs-organism/card/HeroCard";
+export { default as Card } from "@nxs-organism/card/Card";
+export { default as ButtonCancel } from "@nxs-atoms/buttons/ButtonCancel";
+export { default as CopyButton } from "@nxs-molecules/buttons/CopyButton";
+export { default as HintButton } from "@nxs-molecules/buttons/HintButton";
+export { default as PageNotFound } from "@nxs-molecules/errors/PageNotFound";
