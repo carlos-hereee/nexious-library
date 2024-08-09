0eabd6b7a5ec4ed95ec1b538a1c8ac15b69ee624
carlos-hereee <97hernandez.c@gmail.com>
2024-08-08 22:42:46 -0500

    handle title or body are empty

--- src/components/atoms/texts/TextBubble.tsx
+++ src/components/atoms/texts/TextBubble.tsx
@@ -3,8 +3,8 @@ import type { BubbleProps } from "nxs-assets";
 const TextBubble: React.FC<BubbleProps> = ({ title, body }) => {
   return (
     <div className="text-bubble">
-      <h3 className="heading">{title}</h3>
-      <p className="text-body text-max">{body}</p>
+      {title && <h3 className="heading">{title}</h3>}
+      {body && <p className="text-body text-max">{body}</p>}
     </div>
   );
 };
